f3fb5150b6847cadbd94a598c9f33f098dedb81c
Danny Guinther <dguinther@neuralmagic.com>
2022-05-26 13:17:54 -0400

    Add comment about reference hypercorn config

--- hypercorn_conf.py
+++ hypercorn_conf.py
@@ -1,5 +1,8 @@
 import json
 
+# A very solid reference config can be found here:
+# https://github.com/bynect/hypercorn-fastapi-docker/blob/main/images/hypercorn_conf.py
+
 bind = "0.0.0.0:8000"
 
 # This object is just for output. Hypercorn will get it's conf from the primitives defined above.
