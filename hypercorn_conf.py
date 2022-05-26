c68891dd483fc903109c10a36a3f63cb95508dcd
Danny Guinther <dguinther@neuralmagic.com>
2022-05-26 13:14:47 -0400

    Print hypercorn conf when booting hypercorn

--- hypercorn_conf.py
+++ hypercorn_conf.py
@@ -1,1 +1,11 @@
+import json
+
 bind = "0.0.0.0:8000"
+
+# This object is just for output. Hypercorn will get it's conf from the primitives defined above.
+conf_data = {
+    "bind": bind
+}
+
+# Print out the conf for debugging visibility
+print(json.dumps(conf_data, indent = 4), flush = True)
