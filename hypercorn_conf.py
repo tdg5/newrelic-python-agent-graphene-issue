b9d27eebf4ca84ddd571653b233e13c120d76fd1
Danny Guinther <dguinther@neuralmagic.com>
2022-05-26 16:59:21 -0400

    Add script to simplify running locally w/o docker

--- hypercorn_conf.py
+++ hypercorn_conf.py
@@ -5,10 +5,11 @@ import json
 
 bind = "0.0.0.0:8000"
 
-# This object is just for output. Hypercorn will get it's conf from the primitives defined above.
+# This object is just for output. Hypercorn will get it's conf from the
+# primitives defined above.
 conf_data = {
     "bind": bind
 }
 
 # Print out the conf for debugging visibility
-print(json.dumps(conf_data, indent = 4), flush = True)
+print(json.dumps(conf_data, indent=4), flush=True)
