cfe7d750b27f6b004e9e99ea79871110fb3e80ae
Danny Guinther <dguinther@neuralmagic.com>
2022-06-16 14:02:11 +0000

    Add some docstrings and FastAPI configs

--- src/entry.py
+++ src/entry.py
@@ -1,8 +1,18 @@
+"""
+Example/template implementation of an API microservice.
+"""
+
+
 from fastapi import FastAPI
 
-app = FastAPI()
+
+app = FastAPI(
+    description=__doc__,
+    title="hello-full-stack",
+)
 
 
 @app.get("/")
 async def hello_full_stack():
+    """Minimal example GET endpoint."""
     return {"message": "Hello, Full Stack!"}
