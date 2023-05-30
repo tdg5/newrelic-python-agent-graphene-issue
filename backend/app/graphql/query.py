3480f16e5b40c4e2b0e2e23956d09a8299e8dc8b
Danny Guinther <dguinther@neuralmagic.com>
2023-05-30 11:27:43 -0400

    Add inefficient fibonacci test

--- backend/app/graphql/query.py
+++ backend/app/graphql/query.py
@@ -1,5 +1,26 @@
 import graphene
 
+def Fibonacci(n):
+
+    # Check if input is 0 then it will
+    # print incorrect input
+    if n < 0:
+        print("Incorrect input")
+
+    # Check if n is 0
+    # then it will return 0
+    elif n == 0:
+        return 0
+
+    # Check if n is 1,2
+    # it will return 1
+    elif n == 1 or n == 2:
+        return 1
+
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+
 class User(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String()
@@ -9,7 +30,11 @@ class Query(graphene.ObjectType):
     me = graphene.Field(User)
 
     def resolve_me(root, info):
-        return {"id": "john", "name": "John"}
+        return {
+            "id": "john",
+            "name": "John",
+            "fib": Fibonacci(20),
+        }
 
 
 schema = graphene.Schema(query=Query)
